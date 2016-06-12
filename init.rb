93e2142e6b81dd94dbd1b2f8dd3720c463ea5fc8
bilel-kedidi <bilel.kedidi@gmail.com>
2016-06-12 18:18:47 +0100

    add wicked pdf

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wicked_pdf do
+  name 'Redmine Wicked Pdf plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
